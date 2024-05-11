a574b7741a6513f981b625f239bed97a54f60936
oscaradiaz10 <oscar.di@me.com>
2024-05-11 17:31:54 -0500

    Add files via upload

---
+++ src/practice.java
@@ -0,0 +1,11 @@
+
+public class practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Oscar Diaz";
+		
+		System.out.println(name);
+	}
+
+}
